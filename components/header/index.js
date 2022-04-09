9dfcf932ab0958030b1bef79fec30eb53bae6537
Zbigniew Bernacki <38226389+spiggyBe@users.noreply.github.com>
2022-04-09 20:02:51 +0200

    up

--- components/header/index.js
+++ components/header/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Image from 'next/image'
 import logo from '../../public/logo.png'
 import Link from 'next/link'
+import { SearchIcon } from '@heroicons/react/outline'
 
 function Header() {
     return (
-        <header className='grid grid-cols-3 sticky top-0 z-50 bg-white shadow-md p-5'>
+        <header className='grid grid-cols-3 sticky top-0 z-50 bg-white shadow-md p-5 md:px-10'>
             <Link
                 href='/'
                 passHref
@@ -22,7 +23,14 @@ function Header() {
                     <span className='inline-block text-2xl pl-4' >Silesia Real Estate</span>
                 </div>
             </Link>
-            <div></div>
+            <div className='flex items-center md:border-2 rounded-full p-2 md:shadow-sm'>
+                <input
+                    className='flex-grow pl-5 transparent outline-none'
+                    type='text'
+                    placeholder='What are you looking for...'
+                />
+                <SearchIcon className='hidden md:inline-flex h-8 bg-purple-500 text-white rounded-full p-2 cursor-pointer md:mx-2' />
+            </div>
             <div></div>
         </header>
     )
